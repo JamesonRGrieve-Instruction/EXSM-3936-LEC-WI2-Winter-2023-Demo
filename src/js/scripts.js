7edd91729c5ff6f140337e57c2d63207e8331cfb
James Grieve <jgrieve@ualberta.ca>
2023-01-11 14:12:06 -0700

    Starting point;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,91 +1,44 @@
-// Create a class called Person
-class Person {
-    //Implement a greedy, partial and default constructor.
-    
-    constructor()
+class Student {
+    constructor(firstName = "John", middleName = "Anonymous", lastName = "Doe", birthDate = new Date("2000-01-01"))
     {
-        this.firstName = "John";
+        this.id=Math.floor(Math.random()*100000);
+        this.firstName = firstName;
         this.middleName = "Anonymous"
-        this.lastName = "Doe";
-        this.birthDate = new Date("2000-01-01");
-        this.gender = "Male";
-    }
-    /*
-    constructor(firstName, lastName)
-    {
-        this.firstName = firstName;
         this.lastName = lastName;
-        this.birthDate(new Date("2000-01-01"));
+        this.#birthDate = birthDate;
     }
 
-    constructor(firstName, middleName, lastName, birthDate, gender)
-    {
-        this.firstName = firstName;
-        this.middleName = middleName;
-        this.lastName = lastName;
-        this.birthDate(birthDate);
-        this.gender = gender;
-    }
-    */
-
-    // with a first name, middle name, last name and gender field
+    id;
     firstName;
     middleName;
     lastName;
-    gender;
+    get fullName() {
+        return `${this.firstName} ${middleName[0]}. ${this.lastName}`;
+    }
 
-    // and a birth date property
     #birthDate;
     get birthDate() {
         return this.#birthDate;
     }
     set birthDate(x) {
-        // Implement the birth date property to ensure the date is in the past.
         if (x > Date.now())
         {
             throw new Error("Birthdate must be in the past.");
         }
         else
         {
-            this.#birthDate = x;
+            this.birthDate = x;
         }
     }
 
-    // Derive a full name property from the first name, last name and middle initial
-    get fullName() {
-        return `${this.firstName} ${this.middleName[0]}. ${this.lastName}`;
-    }
-
-    // Derive an age property from the birth date.
-    get age() {
-        return new Date(Date.now()).getFullYear()-this.birthDate.getFullYear();
-    }
-
-    // Implement a toString() method that will describe the person in an English sentence.
     toString() {
-        return `A ${this.age} year old person of the ${this.gender} gender, named ${this.fullName}.`;
-    }
-
-    // Implement an introduction() method that will introduce the person by the first name as an English sentence.
-    introduction() {
-        return `Hello, my name is ${this.firstName}!`;
+        return `${this.fullName}, a student with the ID ${person.id}.`;
     }
 }
 
 async function main() {
-    // This is where the code you're actually experimenting with goes.
-    
-    let prompt = "Please enter your name, or 'Exit' to quit: "
-    let name = await input(prompt);
-
-    let person = new Person();
+    let person = new Student(firstName = "Jane");
     output(person);
-
-    while (name != "Exit") 
-    {
-        output("Hello, "+name+"!");
-        name = await input(prompt);
-    }
 }
 
 
