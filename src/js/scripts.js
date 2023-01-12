711cbbaea80a83c92a2c764b88ca9166ef313e00
James Grieve <jgrieve@ualberta.ca>
2023-01-11 20:17:29 -0700

    Object reference fields/properties;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,28 +1,27 @@
-// Create a class called Person
 class Person {
-    //Implement a greedy, partial and default constructor.
-    constructor(firstName = "John", middleName = "Anonymous", lastName = "Doe", birthDate = new Date("2000-01-01"), gender = "Male")
+    constructor(firstName = "John", middleName = "Anonymous", lastName = "Doe", birthDate = new Date("2000-01-01"), gender = "Male", pet = new Pet())
     {
         this.firstName = firstName;
         this.middleName = middleName;
         this.lastName = lastName;
         this.birthDate = birthDate;
         this.gender = gender;
+        // This stores an object of the Pet class.
+        this.pet = pet;
     }
     
-    // with a first name, middle name, last name and gender field
     firstName;
     middleName;
     lastName;
     gender;
+    // This stores an object of the Pet class.
+    pet;
 
-    // and a birth date property
     #birthDate;
     get birthDate() {
         return this.#birthDate;
     }
     set birthDate(x) {
-        // Implement the birth date property to ensure the date is in the past.
         if (x > Date.now())
         {
             throw new Error("Birthdate must be in the past.");
@@ -33,38 +32,43 @@ class Person {
         }
     }
 
-    // Derive a full name property from the first name, last name and middle initial
     get fullName() {
         return `${this.firstName} ${this.middleName[0]}. ${this.lastName}`;
     }
 
-    // Derive an age property from the birth date.
     get age() {
         return new Date(Date.now()).getFullYear()-this.birthDate.getFullYear();
     }
 
-    // Implement a toString() method that will describe the person in an English sentence.
     toString() {
-        return `A ${this.age} year old person of the ${this.gender} gender, named ${this.fullName}.`;
+        return `A ${this.age} year old person of the ${this.gender} gender, named ${this.fullName}. ${this.firstName} owns ${this.pet}.`;
     }
 
-    // Implement an introduction() method that will introduce the person by the first name as an English sentence.
     introduction() {
         return `Hello, my name is ${this.firstName}!`;
     }
 }
 
+class Pet {
+    constructor(type = "Dog", name = "Fido")
+    {
+        this.type = type;
+        this.name = name;
+    }
+
+    type;
+    name;
+
+    toString() {
+        return `A ${this.type} named ${this.name}.`;
+    }
+}
+
 async function main() {
-    // Any arguments not provided that are expected (by order) are assumed to be undefined.
-    // If we want to set (as shown) the first and fifth argument, we can explicitly provide undefined for the 2nd thru 4th.
     let defaultPerson = new Person();
-    let partialPerson1 = new Person("Joey");
-    let partialPerson2 = new Person("Jane", undefined, undefined, undefined, "Female");
-    let greedyPerson = new Person("Bob", "Tom", "Smith", new Date("2005-02-03"), "Male");
+
     output(defaultPerson);
-    output(partialPerson1);
-    output(partialPerson2);
-    output(greedyPerson);
+    output(defaultPerson.pet);
 }
 
 
