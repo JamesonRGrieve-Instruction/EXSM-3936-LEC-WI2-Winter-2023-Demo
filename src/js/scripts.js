e61b6c5738660f0934605142abaa22e24fa14fd3
James Grieve <jgrieve@ualberta.ca>
2023-01-16 20:27:59 -0700

    Arrow functions;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,18 @@
 const clickMe = document.querySelector(".listen");
 const inputField = document.querySelector("input");
 
-clickMe.addEventListener("click", runThis);
-
-function runThis(e) {
+clickMe.addEventListener("click", (e) => {
     console.log(inputField.value);
     inputField.value = "Click!";
+});
+
+// NOT SYNTACTICALLY CORRECT - DOESN'T DEFINE Y
+// PURELY FOR ILLUSTRATION
+function nameDoesntMatter (x) {
+    return y;
 }
+
+(x) => { return y; }
+
+
+(x) => y;
