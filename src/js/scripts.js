a0d9c27e02f2641c3b5a85026d0d28c7c845a701
James Grieve <jgrieve@ualberta.ca>
2023-01-18 19:20:49 -0700

    Bonus;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,12 +1,20 @@
 // Build an array of all of the buttons.
-const buttons = document.querySelectorAll("button");
-
+const buttons = document.querySelectorAll("main button");
+const addButton = document.querySelector("header button");
 // For each of them, add the event listener.
 for (button of buttons)
 {
-    button.addEventListener("click", (event) => {
-        // Based on where I clicked, remove the button I clicked on.
-        event.target.remove();
-    });
+    button.addEventListener("click", deleteSelf);
 }
 
+addButton.addEventListener("click", () => {
+    const newButton = document.createElement("button");
+    newButton.innerText = "Remove Me!";
+    newButton.addEventListener("click", deleteSelf);
+    document.querySelector("main").appendChild(newButton);
+});
+
+function deleteSelf(event) {
+    // Based on where I clicked, remove the button I clicked on.
+    event.target.remove();
+}
