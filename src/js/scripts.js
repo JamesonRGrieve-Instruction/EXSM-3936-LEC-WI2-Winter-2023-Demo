bd7971b2b1ba0a65963d5486af08da53866cc6c7
James Grieve <jgrieve@ualberta.ca>
2023-01-18 19:39:29 -0700

    Breakout room start 1;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,26 @@
-const clickMe = document.querySelector(".listen");
-const inputField = document.querySelector("input");
+// https://cdn.pixabay.com/photo/2022/11/10/20/12/dog-7583611_960_720.jpg
 
-clickMe.addEventListener("click", (e) => {
-    console.log(inputField.value);
-    inputField.value = "Click!";
+/*
+Breakout Room Tasks:
+1) When we click the add button, clear the text field.
+2) When an image is added, store the url in the images array.
+    2a) Store the image in the array as a property of an object (either anonymous, or classed)
+3) When you click an image, remove it (from the page).
+    3a) Also remove it from the array.
+*/
+
+const images = [];
+const addButton = document.querySelector("header button");
+const urlField = document.querySelector("header input");
+
+addButton.addEventListener("click", (event) => {
+    const newImage = document.createElement("img");
+    newImage.src = urlField.value;
+    document.querySelector("main").appendChild(newImage);
 });
 
+// Example of an anonymous object.
+const myObject = {
+    prop1: "Test",
+    prop2: "Again"
+};
