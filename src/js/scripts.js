6a236ee3283011932ffd764182cd2299db4838b7
James Grieve <jgrieve@ualberta.ca>
2023-01-18 19:04:21 -0700

    Branch reset;

--- src/js/scripts.js
+++
@@ -1,8 +0,0 @@
-const clickMe = document.querySelector(".listen");
-const inputField = document.querySelector("input");
-
-clickMe.addEventListener("click", (e) => {
-    console.log(inputField.value);
-    inputField.value = "Click!";
-});
-
