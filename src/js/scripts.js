ebf583209fa7f2a9a1da9bff91b5e001f4cf6170
James Grieve <jgrieve@ualberta.ca>
2023-01-18 20:22:18 -0700

    Breakout room start 2;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,11 +2,11 @@
 
 /*
 Breakout Room Tasks:
-1) When we click the add button, clear the text field.
-2) When an image is added, store the url in the images array.
-    2a) Store the image in the array as a property of an object (either anonymous, or classed)
+
 3) When you click an image, remove it (from the page).
     3a) Also remove it from the array.
+4) Add a second text field in which the user can add "tags" - single words separated by a space that will be stored as a "tags" field of the object in the array (as a string array).
+e.g. tags: ["Cute", "Dog", "Funny"]
 */
 
 const images = [];
@@ -16,6 +16,12 @@ const urlField = document.querySelector("header input");
 addButton.addEventListener("click", (event) => {
     const newImage = document.createElement("img");
     newImage.src = urlField.value;
+    //2) When an image is added, store the url in the images array.
+    //2a) Store the image in the array as a property of an object (either anonymous, or classed)
+    images.push({url: urlField.value});
+    console.log(images);
+    // 1) When we click the add button, clear the text field.
+    urlField.value = "";
     document.querySelector("main").appendChild(newImage);
 });
 
