c9e42e11b2179e227dabe3731220a7fc69bc0d18
James Grieve <jgrieve@ualberta.ca>
2023-01-23 20:52:58 -0700

    Intervals and timers;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,5 @@
 // For each of them, add the event listener.
-for (button of document.querySelectorAll("main button")) button.addEventListener("click", deleteSelf);
+for (button of document.querySelectorAll("main button")) button.addEventListener("click", createTimer);
 
 document.querySelector("header button").addEventListener("click", () => {
     const newButton = document.createElement("button");
@@ -8,7 +8,24 @@ document.querySelector("header button").addEventListener("click", () => {
     document.querySelector("main").appendChild(newButton);
 });
 
-function deleteSelf(event) {
-    // Based on where I clicked, remove the button I clicked on.
-    event.target.remove();
+let activeInterval;
+
+function createTimer(event) {
+    // Clear the interval if it already exists.
+    clearInterval(activeInterval);
+
+    // A timeout runs once (like a timer). Once the timer ends, the associated method will run.
+    setTimeout(() => {
+        console.log("5 second timer has passed!");
+    }, 5000);
+
+    // An interval runs multiple times (like a stopwatch / laps). Every time the interval passes, the associated method will run.
+    activeInterval = setInterval(() => {
+        console.log("2 second interval has passed!");
+    }, 2000);
 }
+
+// Modification of elements with intervals / timers.
+setInterval(() => {
+    document.querySelector("h1").innerText = Number(document.querySelector("h1").innerText)+1;
+}, 1000);
