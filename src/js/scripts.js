daeae31a729be651d3fa34d2934b224a2efef710
James Grieve <jgrieve@ualberta.ca>
2023-01-25 19:15:23 -0700

    Skeleton setup;

---
+++ src/js/scripts.js
@@ -0,0 +1,12 @@
+const messageBox = document.querySelector("#history");
+const inputBox = document.querySelector("#message");
+
+document.querySelector("#send").addEventListener("click", (e) => {
+    e.preventDefault();
+
+});
+
+function genTimestamp() {
+    const currentDate = new Date();
+    
+}
