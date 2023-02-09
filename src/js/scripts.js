46dc7a03e85780e225d4d6bd9520fc2eff9e9d74
James Grieve <jgrieve@ualberta.ca>
2023-02-08 19:08:59 -0700

    Created abstract Shape class; Created 3 child classes; Implemented fields and constructors;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,82 +1,53 @@
-class Person {
-    constructor(firstName = "John", middleName = "Anonymous", lastName = "Doe", birthDate = new Date("2000-01-01"), gender = "Male", pet = new Pet())
+class Shape {
+    constructor(colour = "green")
     {
-        this.firstName = firstName;
-        this.middleName = middleName;
-        this.lastName = lastName;
-        this.birthDate = birthDate;
-        this.gender = gender;
-        // This stores an object of the Pet class.
-        this.pet = pet;
-    }
-    
-    firstName;
-    middleName;
-    lastName;
-    gender;
-    // This stores an object of the Pet class.
-    pet;
-
-    #birthDate;
-    get birthDate() {
-        return this.#birthDate;
-    }
-    set birthDate(x) {
-        if (x > Date.now())
+        if (this.constructor == Shape)
         {
-            throw new Error("Birthdate must be in the past.");
+            throw new Error("Abstract classes cannot be instantiated.");
         }
-        else
-        {
-            this.#birthDate = x;
-        }
+        this.colour = colour;
     }
+    colour;
 
-    get fullName() {
-        return `${this.firstName} ${this.middleName[0]}. ${this.lastName}`;
+    get area() {
+        throw new Error("This abstract property was not implemented.");
     }
-
-    get age() {
-        return new Date(Date.now()).getFullYear()-this.birthDate.getFullYear();
+    get perimeter() {
+        throw new Error("This abstract property was not implemented.");
     }
-
-    toString() {
-        return `A ${this.age} year old person of the ${this.gender} gender, named ${this.fullName}. ${this.firstName} owns ${this.pet}.`;
-    }
-
-    introduction() {
-        return `Hello, my name is ${this.firstName}!`;
+    contain() {
+        throw new Error("This abstract method was not implemented.");
     }
 }
-
-class Pet {
-    constructor(type = "Dog", name = "Fido")
+class Rectangle extends Shape {
+    constructor(length = 5, width = 10, colour = "red")
     {
-        this.type = type;
-        this.name = name;
+        super(colour);
+        this.length = length;
+        this.width = width;
     }
-
-    type;
-    name;
-
-    toString() {
-        return `A ${this.type} named ${this.name}.`;
-    }
+    length;
+    width;
 }
-
+class Triangle extends Shape {
+    constructor(base = 3, height = 5, colour = "blue")
+    {
+        super(colour);
+        this.base = base;
+        this.height = height;
+    }
+    base;
+    height;
+}
+class Circle extends Shape {
+    constructor(radius = 12, colour = "yellow")
+    {
+        super(colour);
+        this.radius = radius;
+    }
+    radius;
+}
 async function main() {
-    let defaultPerson = new Person();
-    output(defaultPerson);
-    // Stringify allows us to convert a JavaScript object into a plaintext representation thereof (JavaScript Object Notation).
-    let jsonText = JSON.stringify(defaultPerson);
-    output(jsonText, "debug");
-    // If we want to then bring that back into JavaScript, we can use Parse.
-    let parsedJSON = JSON.parse(jsonText);
-
-    // Parse will bring the data in, but as JSON does not store methods, if we want that functionality on our import object, we will need to tell JavaScript to what class the imported object belongs.
-    let parsedPerson = Object.assign(new Person(), parsedJSON);
-    parsedPerson.pet = Object.assign(new Pet(), parsedPerson.pet);
-    output(parsedPerson);
 
 }
 
