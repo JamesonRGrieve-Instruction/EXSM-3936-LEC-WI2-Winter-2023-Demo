70f58bd7a1866bbc224c13d11b69c3fe9f96b2d9
James Grieve <jgrieve@ualberta.ca>
2023-02-15 20:11:02 -0700

    Finished persistence;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,17 @@
 // https://cdn.pixabay.com/photo/2022/11/10/20/12/dog-7583611_960_720.jpg
 
 // Set up our array to store our data.
+let readJSON = localStorage.getItem("exsm3936_images");
 let images = [];
+if (readJSON)
+{
+    images = JSON.parse(readJSON);
+    for (image of images)
+    {
+        generateImage(image);
+    }
+}
+
 
 // Set up our references for our text fields and buttons.
 const addButton = document.querySelector("header button");
@@ -14,45 +24,14 @@ addButton.addEventListener("click", (event) => {
     // Prevent the button from refresing the page.
     event.preventDefault();
 
-    const newImageContainer = document.createElement("div");
     // Map our text fields into the array data for future reference.
     const newImageObj = {
         url: urlField.value, 
-        renderedImage: newImageContainer, 
         description: descField.value, 
         tags: tagField.value.split(' ')
     };
-    // Append the image with the src, title and alt attributes set.
-    newImageContainer.appendChild(Object.assign(document.createElement("img"), {src: newImageObj.url, title:newImageObj.description, alt: newImageObj.description}));
-
-    // Alternative to Object.assign():
-    /*
-    const newImage = document.createElement("img");
-    newImage.src = newImageObj.url;
-    newImage.title = newImageObj.description;
-    newImage.alt = newImageObj.description;
-    newImageContainer.appendChild(newImage);
-    */
-
-    // Append the description with the inner text set.
-    newImageContainer.appendChild(Object.assign(document.createElement("p"), {innerText: newImageObj.description}));
-    // Create the span to house the tag links.
-    const newImageTagsContainer = document.createElement("span");
-    // For each tag stored in the tags field of the object, create a new tag link.
-    for (tag of newImageObj.tags)
-    {
-        const newTag = Object.assign(document.createElement("a"), {innerText: `#${tag}`, href:"#"});
-        newTag.addEventListener("click", (e) => {
-            // Pull the hashtag off and set the search term.
-            searchField.value = e.target.innerText.slice(1);
-            // Force the search to occur.
-            filterPage(searchField.value);
-        });
-        newImageTagsContainer.appendChild(newTag);
-    }
-    // Add the tag link container span to the div.
-    newImageContainer.appendChild(newImageTagsContainer);
-
+   
+    generateImage(newImageObj);
     // We're not handling removals this way anymore.
     /*
     newImage.addEventListener("click", (e) => {
@@ -71,9 +50,47 @@ addButton.addEventListener("click", (event) => {
     images.push(newImageObj);
     localStorage.setItem("exsm3936_images", JSON.stringify(images));
 
-    // Add the div to the page.
-    document.querySelector("main").appendChild(newImageContainer);
+
 });
+function generateImage(newImageObj)
+{
+    const newImageContainer = document.createElement("div");
+    newImageObj.renderedImage = newImageContainer; 
+     // Append the image with the src, title and alt attributes set.
+     newImageContainer.appendChild(Object.assign(document.createElement("img"), {src: newImageObj.url, title:newImageObj.description, alt: newImageObj.description}));
+
+     // Alternative to Object.assign():
+     /*
+     const newImage = document.createElement("img");
+     newImage.src = newImageObj.url;
+     newImage.title = newImageObj.description;
+     newImage.alt = newImageObj.description;
+     newImageContainer.appendChild(newImage);
+     */
+ 
+     // Append the description with the inner text set.
+     newImageContainer.appendChild(Object.assign(document.createElement("p"), {innerText: newImageObj.description}));
+     // Create the span to house the tag links.
+     const newImageTagsContainer = document.createElement("span");
+     // For each tag stored in the tags field of the object, create a new tag link.
+     for (tag of newImageObj.tags)
+     {
+         const newTag = Object.assign(document.createElement("a"), {innerText: `#${tag}`, href:"#"});
+         newTag.addEventListener("click", (e) => {
+             // Pull the hashtag off and set the search term.
+             searchField.value = e.target.innerText.slice(1);
+             // Force the search to occur.
+             filterPage(searchField.value);
+         });
+         newImageTagsContainer.appendChild(newTag);
+     }
+     // Add the tag link container span to the div.
+     newImageContainer.appendChild(newImageTagsContainer);
+         // Add the div to the page.
+     document.querySelector("main").appendChild(newImageContainer);
+}
+
+
 // Whenever the text in the input field is modified by keyboard input.
 // We still have to fire this manually if we change the value directly.
 searchField.addEventListener("input", (e) => {
